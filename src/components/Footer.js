14de36fdb7d381a08d650adeb2e49c803082f6fc
benmazouz seif eddine <seifwin74@gmail.com>
2018-09-03 15:24:48 +0200

    m

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ class footer extends Component {
      <div className="container">
         <div className="row">
             <div className="col-md-4"></div>
-            <div className="col-md-4">copy right 2018</div>
+            <div className="col-md-4">copyright&copy;2018</div>
             <div className="col-md-4">
             <a href="https://www.linkedin.com/in/benmazouz-seif-eddine-721112141/"target='_blank'> <FontAwesomeIcon icon={['fab','linkedin-in']}className='mr-2' color='lightgrey'/></a>
             <a href="https://web.facebook.com/profile.php?id=100004879302174&ref=bookmarks" target='_blank'> <FontAwesomeIcon icon={['fab','facebook-f']}className='mr-2' color='lightgrey'/></a>
